61522ed4b75ecef9e26666fcb7f5eeabf52e8266
lixianghui666 <2996314021@qq.com>
2023-05-20 14:41:27 +0800

    update: dist

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,7 @@ export default ({ mode }: { mode: string }) => {
             },
             extensions: ['.tsx', '.ts'],
         },
+        base: './',
         css: {
             preprocessorOptions: {
                 less: {
